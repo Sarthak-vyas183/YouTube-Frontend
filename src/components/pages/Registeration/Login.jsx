317a630962d21fb6fd1c11fa7fc86c8c384be9d0
Sarthak vyas <142726436+Sarthak-vyas183@users.noreply.github.com>
2024-09-30 19:04:32 +0530

    working on login signup functionality with backend

--- src/components/pages/Registeration/Login.jsx
+++ src/components/pages/Registeration/Login.jsx
@@ -1,43 +1,61 @@
-import React, { useContext, useState } from 'react';
-import UserContext from '../../../context/UserContext';
+import React, { useContext, useState } from "react";
+import UserContext from "../../../context/UserContext";
+import axios from "axios";
 
 function Login() {
-  const { token } = useContext(UserContext);
+  const { token, setToken } = useContext(UserContext);
   const [formData, setFormData] = useState({
-    email: '',
-    password: ''
+    email: "",
+    password: "",
   });
 
   const handleChange = (e) => {
-    const { name, value } = e.target; // Correctly accessing name and value
+    const { name, value } = e.target;
     setFormData((prev) => ({
       ...prev,
-      [name]: value // Dynamically update the correct field
+      [name]: value,
     }));
-    console.log(formData.email)
+    console.log(formData.email);
+  };
+
+  const handleSubmit = async () => {
+    try {
+      const response = await axios.post(`/api/v1/user/login`, {
+        email: formData.email,
+        password: formData.password,
+      });
+
+      console.log(response.data);
+
+      setToken(response.data.token);
+    } catch (error) {
+      console.error("Axios error:", error);
+    }
   };
 
   return (
-    <div className='space-y-2'>
+    <div className="space-y-2">
       <div className="flex flex-col space-y-2">
         <input
-          name="email" // Add name attribute
+          name="email"
           value={formData.email}
           onChange={handleChange}
-          className='bg-transparent border-b border-gray-600 pb-2 text-white placeholder-gray-400 focus:outline-none focus:border-blue-500'
+          className="bg-transparent border-b border-gray-600 pb-2 text-white placeholder-gray-400 focus:outline-none focus:border-blue-500"
           type="email"
-          placeholder='Email'
+          placeholder="Email"
         />
         <div className="flex justify-between items-center">
           <input
-            name="password" // Add name attribute
+            name="password"
             value={formData.password}
             onChange={handleChange}
-            className='bg-transparent text-white placeholder-gray-400 focus:outline-none'
+            className="bg-transparent text-white placeholder-gray-400 focus:outline-none"
             type="password"
-            placeholder='Password'
+            placeholder="Password"
           />
-          <button className='text-blue-500 font-medium'>Login</button>
+          <button className="text-blue-500 font-medium" onClick={handleSubmit}>
+            Login
+          </button>
         </div>
       </div>
     </div>
